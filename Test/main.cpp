483add8320ca48a1820a3d1769c915154857fb47
RayShark0605 <gallyonstoavug@gmail.com>
2026-06-03 22:31:36 +0800

    更改main.cpp

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,10 +1,11 @@
 ﻿#include "GeoCrs.h"
+#include "GeoUtility.h"
 
 int main(int argc, char* argv[])
 {
 	GeoCrs crs1 = GeoCrs::FromUserInput("EPSG:4326");
-	GeoCrs crs2;
-	crs2.SetFromEpsg(4326);
+	GeoCrs crs2("WGS84");
+
 	if (crs1 == crs2)
 	{
 		printf("CRS 1 and CRS 2 are the same.\n");
